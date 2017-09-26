b7a92e97ffcbafb65effc4fd370d6d1cfa9ea756
Jordan-JACOBI <32301879+Jordan-JACOBI@users.noreply.github.com>
2017-09-26 16:10:58 +0200

    Add files via upload

---
+++ classes.php
@@ -0,0 +1,40 @@
+<?php
+
+	class Person
+	{
+		
+		public function __construct($firstName, $lastName, $age)
+		{
+			$this->firstName = $firstName;
+			$this->lastName = $lastName;
+			$this->age = $age;
+		}
+		
+		public $firstName;
+		public $lastName;
+		public $age;
+	}
+
+	class Booking
+	{
+		public $price;
+		public $destination;
+		public $warranty;
+		public $people = array();
+		
+		//This function may need a constructor to specify parameters
+		/*public function __construct($price, $destination, $warranty, $people)
+		{
+			$this->price= $price;
+			$this->destination = $destination;
+			$this->warranty= $warranty;
+			$this->people= $people;
+		}*/
+		
+		public function addPerson($person)
+		{
+			array_push($this->people, $person);
+		}
+	}
+
+?>

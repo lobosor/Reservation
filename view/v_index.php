04939ee2895fe58a721d3d3652fbbf09266a8221
lobosor <33683159+lobosor@users.noreply.github.com>
2017-11-15 10:38:39 +0100

    Update v_index.php

--- view/v_index.php
+++ view/v_index.php
@@ -1,4 +1,4 @@
-<html>
+/*<html>
 	<head>
 		<title>Resevation</title>
 		<link rel = "stylesheet" type = "text/css" href ="form.css" >
@@ -17,17 +17,22 @@
 		
 		<form method="post" action="index.php">
 			<p>
-				<?php
+				/*<?php
 				
 					echo new Input("destination","text","Destination : ", new Property("value", $myBooking->getDestination()));
 					echo new Input("numberOfPassengers","number","Nombre de passagers : ", new Property("value", $myBooking->getNumberOfPassengers()));
 					echo new Input("insurance", "checkbox", "Assurance", $myBooking->getInsurance());
 				
-				?>
-				<!--
+				?>*/
 				<label for="destination">Destination : </label>
 				<input id ="destination" type="text" name="destination">
-				<br/>
+					<select name="menu_destination" id="menu_destination_liste">
+					<option value="Paris">Paris</option> 
+					<option value="Amsterdam">Amsterdam</option> 
+     					<option value="Lisbonne">Lisbonne</option> 
+     				</select>
+				<br />
+
 				
 				<label for="numberOfPassengers">Number of passengers : </label>
 				<input id ="numberOfPassengers" type="number" name="numberOfPassengers" min=1>
@@ -35,7 +40,6 @@
 				
 				<label for="insurance">Cancellation insurance : </label>
 				<input id ="insurance" type="checkbox" name="insurance">
-				-->
 			</p>
 			
 			<input type="submit" name="Check" value="Next">
